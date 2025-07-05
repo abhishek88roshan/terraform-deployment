5d70c2e8096ff4f0ac77026dd29be27b0aae9335
EngrAbhishekRoshan <abhishek88roshan@yahoo.com>
2025-07-06 04:11:50 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -8,6 +8,21 @@ output "instance_public_dns" {
   value       = aws_instance.web.public_dns
 }
 
+output "instance_id" {
+  description = "EC2 Instance ID"
+  value       = aws_instance.web.id
+}
+
+output "private_ip" {
+  description = "Private IP of the EC2 instance"
+  value       = aws_instance.web.private_ip
+}
+
+output "availability_zone" {
+  description = "Availability Zone of the subnet"
+  value       = aws_subnet.public.availability_zone
+}
+
 output "vpc_id" {
   description = "VPC ID"
   value       = aws_vpc.main.id
