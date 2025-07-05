e37eae3648a8e1a5be5e400f1e447d3ca785bb3a
EngrAbhishekRoshan <abhishek88roshan@yahoo.com>
2025-07-06 04:01:30 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,29 +1,29 @@
 variable "region" {
   description = "AWS region"
+  type        = string
   default     = "ap-south-1"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
+  type        = string
   default     = "10.0.0.0/16"
 }
 
 variable "subnet_cidr" {
   description = "CIDR block for the public subnet"
+  type        = string
   default     = "10.0.1.0/24"
 }
 
 variable "availability_zone" {
-  description = "Availability zone"
+  description = "Availability zone for subnet"
+  type        = string
   default     = "ap-south-1a"
-}
-
-variable "ami_id" {
-  description = "AMI ID for EC2 instance"
-  default     = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 (update for your region)
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
+  type        = string
   default     = "t2.micro"
 }
