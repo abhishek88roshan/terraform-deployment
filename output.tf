04a862788318c0b7675915175f7e8e067b21db4c
EngrAbhishekRoshan <abhishek88roshan@yahoo.com>
2025-07-06 03:45:35 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,19 @@
+output "instance_public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.web.public_ip
+}
+
+output "instance_public_dns" {
+  description = "Public DNS of the EC2 instance"
+  value       = aws_instance.web.public_dns
+}
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.main.id
+}
+
+output "subnet_id" {
+  description = "Subnet ID"
+  value       = aws_subnet.public.id
+}
